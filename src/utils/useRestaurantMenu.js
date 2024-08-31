258510695017ffc753f872e7b7f4b014e9b4c9ef
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-08-31 18:53:51 -0400

    Menu API update 2

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { MENU_URL } from "./constant";
 
 const useRestaurantMenu = (resId) => {
     const [resInfo, setResInfo] = useState(null);
@@ -10,7 +11,7 @@ const useRestaurantMenu = (resId) => {
     // https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=
 
     const fetchData = async () => {
-        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=" + resId);
+        const data = await fetch(MENU_URL + resId);
         const json = await data.json();
         // console.log("Menu Card", json);
         setResInfo(json.data);
