b3d10fde0d7e61cf9cf0d66237dc790ac3807291
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-08-31 19:19:43 -0400

    Cors for menu API

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -8,9 +8,9 @@ const useRestaurantMenu = (resId) => {
         fetchData();
     }, []);
 
-    // https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=
 
     const fetchData = async () => {
+        // https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=
         const data = await fetch(MENU_URL + resId);
         const json = await data.json();
         // console.log("Menu Card", json);
