f7cdb6400cac31e97824699ffe6ab845a06d7cf9
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-12-10 20:41:45 -0500

    API change

--- src/components/Body.js
+++ src/components/Body.js
@@ -10,22 +10,21 @@ const Body = () => {
     const [listOfRestaurants, setListOfRestaurants] = useState([]);
     const [filteredRestaurant, setFilterdRestaurant] = useState([]);
     const [searchText, setSearchText] = useState("");
-
     const RestaurantCardDiscount = withDiscount(RestaurantCard);
 
     useEffect(() => {
         fetchData();
     }, []);
-    // https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING
+    // https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING
 
 
     const fetchData = async () => {
-        const data = await fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
 
         const json = await data.json();
-        // console.log(json);
-        setListOfRestaurants(json?.data?.cards[1].card?.card?.gridElements?.infoWithStyle?.restaurants);
-        setFilterdRestaurant(json?.data?.cards[1].card?.card?.gridElements?.infoWithStyle?.restaurants);
+        console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+        setListOfRestaurants(json?.data?.cards[4].card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilterdRestaurant(json?.data?.cards[4].card?.card?.gridElements?.infoWithStyle?.restaurants);
     };
 
 
