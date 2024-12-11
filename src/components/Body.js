c12c4024dfce941d64586d4eb6314ce28fed585b
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-12-10 20:46:12 -0500

    API change Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -22,7 +22,6 @@ const Body = () => {
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
 
         const json = await data.json();
-        console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
         setListOfRestaurants(json?.data?.cards[4].card?.card?.gridElements?.infoWithStyle?.restaurants);
         setFilterdRestaurant(json?.data?.cards[4].card?.card?.gridElements?.infoWithStyle?.restaurants);
     };
